51e78a87030a04ab3471fbda38c400ff4953a4fd
Himanshu Dinkar <111561592+yours7himanshu@users.noreply.github.com>
2025-03-23 01:40:52 +0530

    project started

---
+++ lib/ai/models.ts
@@ -0,0 +1,61 @@
+// Define your models here.
+
+export interface Model {
+  id: string;
+  label: string;
+  apiIdentifier: string;
+  description: string;
+}
+
+export const models: Array<Model> = [
+  // {
+  //   id: 'gpt-4o',
+  //   label: 'GPT 4o',
+  //   apiIdentifier: 'gpt-4o',
+  //   description: 'For complex, multi-step tasks',
+  // },
+  // {
+  //   id: 'gpt-4o-mini',
+  //   label: 'GPT 4o Mini',
+  //   apiIdentifier: 'gpt-4o-mini',
+  //   description: 'Affordable for complex, multi-step tasks',
+  // },
+  {
+    id: 'google/gemini-2.0-pro-exp-02-05:free',
+    label: 'google/gemini-2.0-pro-exp-02-05:free',
+    apiIdentifier: 'google/gemini-2.0-pro-exp-02-05:free',
+    description: 'For deep reasoning and complex, multi-step tasks, cheaper.',
+  }
+] as const;
+
+export const reasoningModels: Array<Model> = [
+  // {
+  //   id: 'o1',
+  //   label: 'o1',
+  //   apiIdentifier: 'o1',
+  //   description: 'For deep reasoning and complex, multi-step tasks',
+  // },
+  // {
+  //   id: 'o1-mini',
+  //   label: 'o1-mini',
+  //   apiIdentifier: 'o1-mini',
+  //   description: 'For deep reasoning and complex, multi-step tasks, cheaper.',
+  // },
+  // {
+  //   id: 'o3-mini',
+  //   label: 'o3-mini',
+  //   apiIdentifier: 'o3-mini',
+  //   description: 'For deep reasoning and complex, multi-step tasks, cheaper.',
+  // },
+  {
+    id: 'google/gemini-2.0-pro-exp-02-05:free',
+    label: 'google/gemini-2.0-pro-exp-02-05:free',
+    apiIdentifier: 'google/gemini-2.0-pro-exp-02-05:free',
+    description: 'For deep reasoning and complex, multi-step tasks, cheaper.',
+  }
+] as const;
+
+export const DEFAULT_MODEL_NAME: string = 'google/gemini-2.0-pro-exp-02-05:free';
+export const DEFAULT_REASONING_MODEL_NAME: string = 'google/gemini-2.0-pro-exp-02-05:free';
+
+
